f71add625bed6332c14ebaca585d44b75969506f
Vaani_Bhatt <1052004@lwsd.org>
2026-05-27 22:00:03 -0700

    temporarily removed

--- script.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-canvas.width = window.innerWidth - 20
-canvas.height = window.innerHeight - 20
-
-c.fillRect(0, 0, canvas.width, canvas.height)
